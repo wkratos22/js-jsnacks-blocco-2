1ab202b9a70b8dd1f7ff2018dbd0401543ff8264
wkratos22 <michaelwachnicki2001@gmail.com>
2022-03-16 14:11:40 +0100

    esercizio 1 finito

---
+++ esercizio1/main.js
@@ -0,0 +1,22 @@
+// let num1 = prompt("Scrivi il primo numero");
+// let num2 = prompt("Scrivi il secondo numero");
+
+// if(){
+
+// }
+
+
+ var num1;                
+ var num2 = 3;
+
+ num1=prompt("Inserici un numero intero")*1;
+        
+ /*Verifico se num1 è pari o dispari*/
+ if (num1%2==0)
+  {
+   alert("Il numero è pari:" +" "+ num1);
+  }
+ else
+  {
+   alert("Il numero è dispari" +" "+ num2);
+  }
